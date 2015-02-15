8a5f6ef0a14d53c3fc0c4d88c745f08b9f8d5b1d
ssemakin <ssemakin@gmail.com>
2015-02-15 21:40:18 +0200

    add all objects

--- src/tetris/objects.clj
+++ src/tetris/objects.clj
@@ -1,5 +1,25 @@
 (ns tetris.objects)
 
+(def i-like
+    #{{:x 0 :y 0} {:x 1 :y 0} {:x 2 :y 0} {:x 3 :y 0}})
+
+(def j-like 
+    #{{:x 0 :y 0} {:x 1 :y 0} {:x 2 :y 0} {:x 0 :y 1}})
+
+(def l-like 
+    #{{:x 0 :y 0} {:x 1 :y 0} {:x 2 :y 0} {:x 2 :y 1}})
+
 (def o-like
     #{{:x 0 :y 0} {:x 1 :y 0} {:x 0 :y 1} {:x 1 :y 1}})
 
+(def s-like 
+    #{{:x 0 :y 0} {:x 1 :y 0} {:x 1 :y 1} {:x 2 :y 2}})
+
+(def t-like 
+    #{{:x 0 :y 0} {:x 1 :y 0} {:x 2 :y 0} {:x 1 :y 1}})
+
+(def z-like 
+    #{{:x 0 :y 1} {:x 1 :y 1} {:x 1 :y 0} {:x 2 :y 0}})
+
+(def all-objects 
+  [i-like j-like l-like o-like s-like t-like z-like])
