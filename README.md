5d2425c9da0b8c1c2c57adc2665c42239f0d9e7d
ssemakin <ssemakin@gmail.com>
2015-02-15 14:15:13 +0200

    add simple test

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Dependencies
 
-## clojure.tools.namespace
+## Refreshing: clojure.tools.namespace
 
 Use the following command to import *refresh*
 
@@ -12,4 +12,12 @@ In order to refresh repl space:
     $ user=> (refresh)
 
 See for more info: https://github.com/clojure/tools.namespace
+
+
+## Tests: clojure.test 
+
+Running Clojure tests
+
+    $ user=> (run-tests)
+
 
--- src/tetris/core.clj
+++ src/tetris/core.clj
@@ -1,11 +1,18 @@
 (ns tetris.core
+  (require [clojure.tools.namespace.repl :refer [refresh]])
+  (use [clojure.test])
   (:gen-class))
+
 
 (def o-like
     #{{:x 0 :y 0} {:x 1 :y 0} {:x 0 :y 1} {:x 1 :y 1}})
 
 (defn place-to [x y obj]
-  (map #(hash-map :x (+ (:x %) x) :y (+ (:y %) y)) obj))
+  (set
+    (map #(hash-map :x (+ (:x %) x) :y (+ (:y %) y)) obj)))
+
+(deftest placing-objects
+  (is (= #{{:x 10 :y 10} {:x 11 :y 10} {:x 10 :y 11} {:x 11 :y 11}} (place-to 10 10  o-like))))
 
 (defn -main
   "I don't do a whole lot ... yet."
