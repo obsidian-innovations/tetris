a70e6d4ca4427c6f1ab7fa2bc83208d113de4aad
ssemakin <ssemakin@gmail.com>
2015-02-16 22:58:33 +0200

    add chime lib to handle scheduling

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-;  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [jarohen/chime "0.1.6"]]
   :plugins [[lein-auto "0.1.1"]]
   :main ^:skip-aot tetris.core
   :target-path "target/%s"
