c26199ce421e77d3ac14797066242689e5856e59
ssemakin <ssemakin@gmail.com>
2015-02-23 23:44:33 +0200

    adding init-bricks

--- src/tetris/board.clj
+++ src/tetris/board.clj
@@ -3,15 +3,16 @@
 (def boundaries {:bottom-y 0
                  :top-y 0
                  :left-x 0
-                 :right-x 0})
+                 :right-x 0
+                 :wall-bricks #{}})
 
 (def tetromino {:x 0
                 :y 0
                 :positions []})
 
-(def state {:boundaries boundaries
-            :tetromino tetromino
-            :heap #{}})
+(defn state {:boundaries boundaries
+             :tetromino tetromino
+             :heap #{}})
 
 (defn update-positions-in [s positions]
   (update-in s [:tetromino :positions] positions))
@@ -22,3 +23,5 @@
 (defn shift-positions-left [s]
   (update-in s [:tetromino :positions] #(conj (subvec % 1) (first %))))
 
+(defn init-bricks [b]
+  )
