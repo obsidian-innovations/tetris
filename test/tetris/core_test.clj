b0411b52f553fce1c809cb7b869f4dde6205f577
ssemakin <ssemakin@gmail.com>
2015-03-13 18:38:29 +0200

    fix old test

--- test/tetris/core_test.clj
+++ test/tetris/core_test.clj
@@ -3,13 +3,13 @@
             [tetris.tetromino :refer :all]
             [tetris.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
 
 (deftest testing-line-mask
   (is (= #{{:x 0 :y 5} {:x 1 :y 5} {:x 2 :y 5}} (line-mask 0 2 5))))
 
 (deftest moving-objects
-  (is (= #{{:x 10 :y 10} {:x 11 :y 10} {:x 10 :y 11} {:x 11 :y 11}} (move-to-xy 10 10 o-like))))
+  (is (= #{{:x 10 :y 10} {:x 11 :y 10} {:x 10 :y 11} {:x 11 :y 11}} (move-to-xy 10 10 (first o-like)))))
 
