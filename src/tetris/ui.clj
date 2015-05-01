0921b31c86d335e63ef6c9e7fdc41895f8af8c69
ssemakin <ssemakin@gmail.com>
2015-05-01 21:30:21 +0300

    break-down printing to screen to several functions

--- src/tetris/ui.clj
+++ src/tetris/ui.clj
@@ -3,6 +3,7 @@
     [chime :refer [chime-at]]
     [clj-time.core :as t]
     [lanterna.screen :as term :refer :all]
+    [tetris.actions.tetromino :refer [move-to-coords]]
     [tetris.actions.common :refer [move-to-xy]]
     [tetris.actions.events :refer :all]
     [tetris.actions.game :refer :all]
@@ -16,14 +17,41 @@
    :down :rotate-clockwise
    :enter :move-down})
 
+(defn- convert-coords-to-screen [game coords]
+  (let [top-y (:top-y (:walls game))]
+    (map #(hash-map :x (* (:x %) 2) :y (- top-y (:y %))) coords)))
+
+(defn- print-tetromino-to-screen [screen game]
+  (let [coords (move-to-coords game)
+        bricks (convert-coords-to-screen game coords)]
+    (doall (map #(term/put-string screen (:x %) (:y %) "▒░") bricks))
+    ))
+
+(defn- print-heap-to-screen [screen game]
+  (let [coords (get-in game [:heap])
+        bricks (convert-coords-to-screen game coords)]
+    (doall (map #(term/put-string screen (:x %) (:y %) "▒░") bricks))
+    ))
+
+(defn- print-walls-to-screen [screen game]
+  (let [coords (get-in game [:walls :wall-bricks])
+        bricks (convert-coords-to-screen game coords)]
+    (doall (map #(term/put-string screen (:x %) (:y %) "▓▓") bricks))
+    ))
+
 ;http://www.rapidtables.com/code/text/ascii-table.htm
-(defn print-game-to-screen [game screen]
-  (term/clear screen)
-  (doall
-    (map
-      #(term/put-string screen (* (:x %) 2) (- (:top-y (:walls game)) (:y %)) "▒░")
-      (bricks game)))
-  (term/redraw screen))
+(defn- print-game-to-screen [game screen]
+  (let []
+    (term/clear screen)
+    (print-walls-to-screen screen game)
+    (print-heap-to-screen screen game)
+    (print-tetromino-to-screen screen game)
+    ;(doall
+    ;  (map
+    ;    #(term/put-string screen (* (:x %) 2) (- (:top-y (:walls game)) (:y %)) "▒░")
+    ;    (bricks game)))
+    (term/redraw screen)
+    ))
 
 (defn schedule-next-move [print-game-fn get-key-fn stop-game-fn game]
   (chime-at [(-> 40 t/millis t/from-now)]
@@ -44,6 +72,8 @@
         get-key-fn #(term/get-key screen)
         stop-fn #(term/stop screen)]
     (term/start screen)
+    ;(term/clear screen)
+    ;(print-walls-to-screen screen board)
     (schedule-next-move print-game-fn get-key-fn stop-fn board)))
 
 (defn -main [& args]
