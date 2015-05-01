ebdf7b0942a776a25e9da44e5cbbf29b8996497c
ssemakin <ssemakin@gmail.com>
2015-05-02 00:39:08 +0300

    optimize game drowing: redraw only moving parts, no scren clear on each move

--- src/tetris/ui.clj
+++ src/tetris/ui.clj
@@ -21,16 +21,33 @@
   (let [top-y (:top-y (:walls game))]
     (map #(hash-map :x (* (:x %) 2) :y (- top-y (:y %))) coords)))
 
-(defn- print-tetromino-to-screen [screen game]
-  (let [coords (move-to-coords game)
-        bricks (convert-coords-to-screen game coords)]
-    (doall (map #(term/put-string screen (:x %) (:y %) "▒░") bricks))
+(defn- print-tetromino-to-screen! [screen game coords charset]
+  (let [bricks (convert-coords-to-screen game coords)]
+    (doall (map #(term/put-string screen (:x %) (:y %) charset) bricks))
     ))
 
-(defn- print-heap-to-screen [screen game]
-  (let [coords (get-in game [:heap])
-        bricks (convert-coords-to-screen game coords)]
-    (doall (map #(term/put-string screen (:x %) (:y %) "▒░") bricks))
+(defn- animate-tetromino-to-screen! [screen game game-old]
+  (let [coords-old (move-to-coords game-old)
+        coords (move-to-coords game)]
+    (if-not (= coords-old coords)
+      (do
+        (print-tetromino-to-screen! screen game-old coords-old "  ")
+        (print-tetromino-to-screen! screen game coords "▒░")))
+    ))
+
+(defn- print-heap-to-screen! [screen game coords charset]
+  (let [bricks (convert-coords-to-screen game coords)]
+    (doall (map #(term/put-string screen (:x %) (:y %) charset) bricks))
+    ))
+
+(defn- animate-heap-to-screen! [screen game game-old]
+  (let [coords (:heap game)
+        coords-old (:heap game-old)]
+    (if-not (= coords-old coords)
+      (do
+        (print-heap-to-screen! screen game-old coords-old "  ")
+        (print-heap-to-screen! screen game coords "▒░")
+        ))
     ))
 
 (defn- print-walls-to-screen [screen game]
@@ -40,16 +57,10 @@
     ))
 
 ;http://www.rapidtables.com/code/text/ascii-table.htm
-(defn- print-game-to-screen [game screen]
+(defn- print-game-to-screen [screen game game-old]
   (let []
-    (term/clear screen)
-    (print-walls-to-screen screen game)
-    (print-heap-to-screen screen game)
-    (print-tetromino-to-screen screen game)
-    ;(doall
-    ;  (map
-    ;    #(term/put-string screen (* (:x %) 2) (- (:top-y (:walls game)) (:y %)) "▒░")
-    ;    (bricks game)))
+    (animate-tetromino-to-screen! screen game game-old)
+    (animate-heap-to-screen! screen game game-old)
     (term/redraw screen)
     ))
 
@@ -62,18 +73,18 @@
         (if (= key-pressed :escape)
           (stop-game-fn)
           (do
-            (print-game-fn game-updated)
+            (print-game-fn game-updated game)
             (schedule-next-move print-game-fn get-key-fn stop-game-fn game-updated)))))))
 
 (defn start-game []
   (let [board (state/init-state)
         screen (term/get-screen)
-        print-game-fn #(print-game-to-screen % screen)
+        print-game-fn #(print-game-to-screen screen %1 %2)
         get-key-fn #(term/get-key screen)
         stop-fn #(term/stop screen)]
     (term/start screen)
-    ;(term/clear screen)
-    ;(print-walls-to-screen screen board)
+    (term/clear screen)
+    (print-walls-to-screen screen board)
     (schedule-next-move print-game-fn get-key-fn stop-fn board)))
 
 (defn -main [& args]
