8f0f8fc5b9ff81e9039737effe622eefe51e87d7
ssemakin <ssemakin@gmail.com>
2015-05-07 21:41:45 +0300

    code clean up

--- src/tetris/ui.clj
+++ src/tetris/ui.clj
@@ -27,8 +27,7 @@
 
 (defn- print-tetromino-to-screen! [screen game coords charset]
   (let [bricks (convert-coords-to-screen screen game coords)]
-    (doall (map #(term/put-string screen (:x %) (:y %) charset) bricks))
-    ))
+    (doall (map #(term/put-string screen (:x %) (:y %) charset) bricks))))
 
 (defn- animate-tetromino-to-screen! [screen game game-old]
   (let [coords-old (move-to-coords game-old)
@@ -36,8 +35,7 @@
     (if-not (= coords-old coords)
       (do
         (print-tetromino-to-screen! screen game-old coords-old "  ")
-        (print-tetromino-to-screen! screen game coords "▒░")))
-    ))
+        (print-tetromino-to-screen! screen game coords "▒░")))))
 
 (defn- print-next-tetromino!
   ([screen game coords]
@@ -63,8 +61,7 @@
 
 (defn- print-heap-to-screen! [screen game coords charset]
   (let [bricks (convert-coords-to-screen screen game coords)]
-    (doall (map #(term/put-string screen (:x %) (:y %) charset) bricks))
-    ))
+    (doall (map #(term/put-string screen (:x %) (:y %) charset) bricks))))
 
 (defn- animate-heap-to-screen! [screen game game-old]
   (let [coords (:heap game)
@@ -72,15 +69,12 @@
     (if-not (= coords-old coords)
       (do
         (print-heap-to-screen! screen game-old coords-old "  ")
-        (print-heap-to-screen! screen game coords "▒░")
-        ))
-    ))
+        (print-heap-to-screen! screen game coords "▒░")))))
 
 (defn- print-walls-to-screen! [screen game]
   (let [coords (get-in game [:walls :wall-bricks])
         bricks (convert-coords-to-screen screen game coords)]
-    (doall (map #(term/put-string screen (:x %) (:y %) "▓▓") bricks))
-    ))
+    (doall (map #(term/put-string screen (:x %) (:y %) "▓▓") bricks))))
 
 (defn- print-completed-lines! [screen game]
   (let [coords [{:x (+ (:board-width config/main) 6) :y (- (:top-y (:walls game)) 5)}]
@@ -92,7 +86,7 @@
         level (:level (get-level (:completed-lines-count (:stats game))))]
     (print-tetromino-to-screen! screen game coords (str "Level:         " level))))
 
-(defn- animate-current-level! [screen game game-old]
+(defn- print-current-level-if-changed! [screen game game-old]
   (when (lines-cleared? game game-old)
     (print-current-level! screen game)))
 
@@ -102,7 +96,7 @@
     (animate-next-tetromino-to-screen! screen game game-old)
     (animate-tetromino-to-screen! screen game game-old)
     (animate-heap-to-screen! screen game game-old)
-    (animate-current-level! screen game game-old)
+    (print-current-level-if-changed! screen game game-old)
     (print-completed-lines! screen game)
     (term/redraw screen)))
 
